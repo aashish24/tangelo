7011505a2229b371ec4271922152efe0b01a099d
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-06-18 10:12:33 -0400

    Cache location to latlon

--- data-processing/kiva-lenders-geolocation.py
+++ data-processing/kiva-lenders-geolocation.py
@@ -16,6 +16,7 @@ def repl_func(m):
 
 geonames = pymongo.Connection("localhost")["xdata"]["geonames"]
 lenders = pymongo.Connection("localhost")["xdata"]["kiva.lenders"]
+cache = {}
 
 # Read location information from lenders database and using geonames
 # get the lat/lon of that location, and finally add it to the lenders database
@@ -30,19 +31,24 @@ for entry in lenders.find({ }, { "_id": 1, "lenders:country_code": 1, "lenders:w
     # Print info on inputs
     #print 'id %s country_code %s place %s state %s' % (oid, country, location, state)
 
-    # Use only the first matched result
-    result = list(geonames.find({ "country code": country, "place name": location, "admin code1": state},
-        { "_id": 0, "latitude": 1, "longitude": 1 }))
-
-    # print 
+    key = (country, state, location)
     latlon_pair = ""
-    if len(result) > 0:
-        #print 'type of id', type(id)
-        #print 'type of id', type(latlon_pair)
 
-        latlon_pair = str(result[0]["latitude"]) + " " + str(result[0]["longitude"])
+    if key in cache:
+        latlon_pair = cache[key]
+    else:
+        # Use only the first matched result
+        result = list(geonames.find({ "country code": country, "place name": location, "admin code1": state},
+            { "_id": 0, "latitude": 1, "longitude": 1 }))
+
+        if len(result) > 0:
+            #print 'type of id', type(id)
+            #print 'type of id', type(latlon_pair)
+            latlon_pair = str(result[0]["latitude"]) + " " + str(result[0]["longitude"])
+
     lenders.update({ "_id": ObjectId(oid) },{ "$set": { "loans:location:geo:pairs":latlon_pair } })
     print latlon_pair
+    cache[key] = latlon_pair
 
 
 # sys.stderr.write("== %d blanks" % blank)
